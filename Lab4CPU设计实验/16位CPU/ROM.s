e966a292dc8a3945d22bd8bafcc7e365bf1a6ebe
lumos <630212894@qq.com>
2017-12-25 13:28:09 +0800

    1

---
+++ Lab4CPU设计实验/16位CPU/ROM.s
@@ -0,0 +1,13 @@
+lw r0,r0,20                
+lw r1,r1,21               
+add r0,r1,r2               
+sub r0,r1,r2              
+and r0,r1,r2              
+or r0,r1,r2                
+sw r0, r2,20             
+beq r0, r1,20              
+bne r0, r1,20             
+ori r0,r3,1              
+addi r0,r3,2               
+andi r0,r3,1               
+jump  0
