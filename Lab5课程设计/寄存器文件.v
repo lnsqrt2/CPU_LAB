2b516fca963e94d752cfc5ffbbe1a0e9cbcc6f85
lumos <630212894@qq.com>
2018-02-26 11:16:37 +0800

    a

--- Lab5课程设计/寄存器文件.v
+++ Lab5课程设计/寄存器文件.v
@@ -1,5 +1,27 @@
-module regfile()
-wire;
-reg test[1:0];
+module regfile(clk, WE, rW, rA, rB, W, A, B)
+
+input clk, WE;//write enable control
+input [4:0] rW, rA, rB;//register addr
+input [31:0] W;//data to rW
+output [31:0] A, B;//data from rA,rB
+
+reg [31:0] A, B;
+reg [31:0] register [31:0];//32 registers
+
+initial
+	begin
+		register[0] = 32'b0;//No.0 register
+	end
+
+assign A = register[rA];
+assign B = register[rB];
+
+always @(posedge clk)
+	begin
+		if ((rW != 0) && (WE == 1))
+			begin
+				register[rW] <= W;
+			end
+	end
 
 endmodule
